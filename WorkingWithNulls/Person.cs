b6186480f85d07b04f78fa1493034394ed9a4ee3
Viktor Svizhak <viktorsvizhak@live.com>
2019-05-15 16:46:48 +0300

    Add project files.

---
+++ WorkingWithNulls/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkingWithNulls
+{
+    class Person
+    {
+        public int? Lvl;
+        public string Name;
+        public bool? B;
+        public Person()
+        {
+        }
+    }
+}
---
+++ WorkingWithNulls/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WorkingWithNulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[3]
+            {
+                new Person(){Name = "Sarah"},
+                new Person(),
+                null
+            };
+            //int b = (int)person.Lvl;
+            //Person[] person=null;
+            var t1 = person?[0]?.Name;
+            var t2 = person?[1]?.Name;
+            var t3 = person?[2]?.Name;
+            var t4 = person[0]?.Name;
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            //Console.WriteLine(person.B.GetValueOrDefault());
+        }
+    }
+}
